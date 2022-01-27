7e61e7609eb4a896f0ee9ab3caebe2cf2397d7f5
waa <zengchh3@gmail.com>
2022-01-27 17:39:39 +0800

    update fetch_filename_list

--- fetch_filename_list.py
+++ fetch_filename_list.py
@@ -9,61 +9,71 @@
 #
 ###############################################################################
 
-import json
 import os
 import os.path as osp
 
+import yaml
 
-def fetch_filename_list(root):
+
+def fetch_filename_list(data_root):
     """Fetch filename list and sort by cardinality in decreasing order.
 
     Args:
         root: the root directory of data.
     """
 
-    dir2fnames = dict()
+    print("Getting filenames from root directory...")
 
-    print("Getting filenames from root...")
+    class_names = os.listdir(data_root)
+    class2fnames = dict()
+    class2fnum = dict()
 
-    for root, dirs, files in os.walk(root):
-        for dir_name in dirs:
-            dir_path = osp.join(root, dir_name)
-            fnames = [
-                osp.join(dir_name, fname) for fname in os.listdir(dir_path)
+    for class_name in class_names:
+        dir_path = osp.join(data_root, class_name)
+        fnames = [
+            osp.join(class_name, fname) for fname in os.listdir(dir_path)
 
-                if osp.splitext(fname)[1] in ['.jpeg', '.jpg']
-            ]
-            dir2fnames[dir_name] = fnames
+            if osp.splitext(fname)[1] in ['.jpeg', '.jpg']
+        ]
+        class2fnames[class_name] = fnames
+        class2fnum[class_name] = len(fnames)
 
-    dir2fnum = {
-        dir_name: len(fnames)
+    sorted_class2fnum = sorted(class2fnum.items(),
+                               key=lambda kv: kv[1],
+                               reverse=True)
 
-        for dir_name, fnames in dir2fnames.items()
-    }
+    label2fnames = dict()
+    label2class = dict()
 
-    print(f"Number of directories:\n {dir2fnum}")
+    for i, (class_name, fnum) in enumerate(sorted_class2fnum):
+        print(f"{i:>2}: {class_name:>4} : {fnum:>5}")
+        label2fnames[i] = class2fnames[class_name]
+        label2class[i] = class_name
 
-    sorted_dir_list = sorted(dir2fnum[0], key=lambda d: d[1], reverse=True)
-
-    print(f"Sorted directory list: {sorted_dir_list}")
-
-    label2fnames = {}
-
-    for i, dir_name in enumerate(sorted_dir_list):
-        fnames = dir2fnames[dir_name]
-        label2fnames[i] = fnames
-
-    print(f"label2fnames:\n{label2fnames}")
-
-    return label2fnames
+    return label2fnames, label2class
 
 
 if __name__ == "__main__":
-    root = "~/Datasets/Xray14/Data/"
-    save_path = "~/Datasets/Xray14/label2fnames.json"
+    root_dir = osp.expanduser("~/Datasets/ISIC2019/Data")
+    label2fnames_path = osp.expanduser("~/Datasets/ISIC2019/label2fnames.yaml")
+    label2class_path = osp.expanduser("~/Datasets/ISIC2019/label2class.yaml")
 
-    label2fnames = fetch_filename_list(root)
+    label2fnames, label2class = fetch_filename_list(root_dir)
 
-    print(f"Writing into '{save_path}'...")
-    with open(save_path, 'w') as f:
-        json.dump(label2fnames, f)
+    print(f"Writing label2fnames into '{label2fnames_path}'...")
+    with open(label2fnames_path, 'w') as f1:
+        yaml.dump(
+            label2fnames,
+            f1,
+            allow_unicode=True,
+            default_flow_style=False,
+        )
+
+    print(f"Writing label2class into '{label2class_path}'...")
+    with open(label2class_path, 'w') as f2:
+        yaml.dump(
+            label2class,
+            f2,
+            allow_unicode=True,
+            default_flow_style=False,
+        )
